92ea26990fc782f02fa1237ef6b9124dd6e3aec5
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-10-01 19:07:32 +0500

    js basic programs practice done

---
+++ app.js
@@ -0,0 +1,55 @@
+// Head & Tail Program:
+
+
+// var headUser = prompt("Enter head username:");
+
+// var tailUser = prompt("Enter tail username:");
+
+// var toss = Math.round(Math.random() * 1);
+
+
+// if ( toss === 1 ) {
+//     alert( headUser + " won " );
+// }
+// else {
+//     alert( tailUser + " won " );
+// }
+
+
+
+
+// Dice Number Generator Program:
+
+
+// var dice = Math.ceil(Math.random() * 6);
+
+// console.log(dice);
+
+
+
+
+// Random Password Generator Program:
+
+
+// function randompasswordgenerator() {
+    
+//     var pass = "";
+
+//     var str = "abcdefghijklmnopqrstuvwxyz!@#$%&*)(-_abcdefghijklmnopqrstuvwxyz0123456789";
+
+//     var passLength = +prompt("Enter password length:");
+    
+//     for ( var i = 1 ; i <= passLength ; i++ ) {
+//         var char = Math.floor(Math.random() * str.length + 1);
+//         pass += str.charAt(char)
+//     }
+
+//     return pass;
+// }
+
+// console.log(randompasswordgenerator());
+
+
+
+
+// -----------------------------------------------------------------------------------------
